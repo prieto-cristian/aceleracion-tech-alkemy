b8361011b1da021ea1343e4f377dcadfef4f313d
Cristian Prieto <pcristiandev@gmail.com>
2026-07-28 20:28:33 -0300

    Colocar un HOLA MUNDO al visitar home

--- modulo-3-django/clase-django-1/proyecto_clase/urls.py
+++ modulo-3-django/clase-django-1/proyecto_clase/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+def hola_mundo(request):
+    return HttpResponse("<h1>HOLA MUNDO</h1>")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", hola_mundo)
 ]
