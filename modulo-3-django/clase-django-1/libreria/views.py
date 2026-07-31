5b43a1e5ea598b252a700964f68912e087a0d950
Cristian Prieto <pcristiandev@gmail.com>
2026-07-31 00:58:33 -0300

    Traer libros de DB y mostrarlos al visitar libreria/

--- modulo-3-django/clase-django-1/libreria/views.py
+++ modulo-3-django/clase-django-1/libreria/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def mostrar_libros(request):
+    """Trae de la DB todos los libros cargados"""
+    libros = models.Libro.objects.all()
+    if libros:
+        return HttpResponse(libros)
+    else:
+        return HttpResponse("No hay libros disponibles en este momento")
