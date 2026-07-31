37686a02443e4f41d935903d12fb24d96143e7d5
Cristian Prieto <pcristiandev@gmail.com>
2026-07-31 00:32:20 -0300

    Crear el Model Autor con 2 campos

--- modulo-3-django/clase-django-1/libreria/models.py
+++ modulo-3-django/clase-django-1/libreria/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30, null=False, blank=False)
+    fecha_nacimiento = models.DateField()
