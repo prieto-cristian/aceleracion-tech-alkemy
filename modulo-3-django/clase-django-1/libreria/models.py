7071bb73a51529ebbe7927af1049e4f983c1ff84
Cristian Prieto <pcristiandev@gmail.com>
2026-07-31 00:48:29 -0300

    Agregar el modelo Libro

--- modulo-3-django/clase-django-1/libreria/models.py
+++ modulo-3-django/clase-django-1/libreria/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.db.models import CASCADE
+
 
 # Create your models here.
 class Autor(models.Model):
     nombre = models.CharField(max_length=30, null=False, blank=False)
     fecha_nacimiento = models.DateField()
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200, null=False, blank=False)
+    isbn = models.CharField()
+    fecha_publicacion = models.DateField()
+    escrito_por = models.ForeignKey(Autor, on_delete=CASCADE,related_name="libros")
+    paginas = models.IntegerField()
